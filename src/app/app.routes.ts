ac44445228d47a1e5637760f72069de0abce4d61
floraprvt <f.preuvot@live.fr>
2026-02-19 13:36:10 +0100

    feat(auth): add guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-    { path: '', component: Home },
+    { path: '', component: Home, canActivate: [authGuard] },
     { path: 'connexion', component: Login },
     { path: 'inscription', component: Register }
 ];
