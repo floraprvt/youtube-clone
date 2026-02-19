51d96183206a5500af40f7052c670aeba036fd5b
floraprvt <f.preuvot@live.fr>
2026-02-19 11:47:27 +0100

    feat(general): add login + register + logout in pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-    { path: '', component: Home }
+    { path: '', component: Home },
+    { path: 'connexion', component: Login },
+    { path: 'inscription', component: Register }
 ];
